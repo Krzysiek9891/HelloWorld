99ff5fb18774a18c78e1edbc2d3bb09ed51eb3a9
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2019-06-16 13:56:04 +0200

    New print method.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 public class Main {
+
     public static void main(String[] args) {
 
         String localMessage="Hello world!";
 
         print(localMessage);
+
+        Print print = new Print("Hello");
+
+        System.out.println(print.handlePrint());
     }
 
     public static void print(String message){
         System.out.println(message);
     }
+
+
 }
+
---
+++ src/Print.java
@@ -0,0 +1,11 @@
+public class Print {
+    private String message;
+
+    public Print(String message){
+        this.message = message;
+    }
+
+    public Print handlePrint(){
+        return this;
+    }
+}
